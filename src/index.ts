6a538d52e1a39e11abaef653afee5e2bc9d182e3
fabasoad <fabasoad@gmail.com>
2025-05-08 00:15:52 +0900

    Test webhook 4

--- src/index.ts
+++ src/index.ts
@@ -9,22 +9,46 @@ async function run() {
   // const jsonString = await fs.readFile(sarifPath, 'utf8')
   // const sarif = JSON.parse(jsonString)
   const webhook = new IncomingWebhook(webhookUrl)
+
+  const repo = 'wp-appsec/github-actions-dashboard'
+  const analysisType = 'IaC analysis'
+  const critical = 0
+  const high = 0
+  const medium = 3
+  const low = 6
+  const info = 0
+  const jobUrl = 'https://job-url.com'
+  const jobId = '12345'
   const { text } = await webhook.send({
     blocks: [
       {
-        type: 'rich_text',
+        type: 'header',
+        text: {
+          type: 'plain_text',
+          text: 'yevhen-fabizhevskyi_stargate'
+        }
+      },
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `*${repo}*\n_${analysisType}_\nCRITICAL: ${critical}, HIGH: ${high}, MEDIUM: ${medium}, LOW: ${low}, INFO: ${info}\nJob <${jobUrl}|${jobId}>`,
+        },
+      },
+      {
+        type: 'context',
         elements: [
           {
-            type: 'rich_text_section',
-            elements: [
-              {
-                type: 'color',
-                value: color
-              }
-            ]
-          }
-        ]
-      }
+            type: 'mrkdwn',
+            text: 'Production Infrastructure Security team - @prodsec',
+          },
+          {
+            type: 'image',
+            image_url: 'https://cdn-icons-png.flaticon.com/512/9070/9070006.png',
+            alt_text: 'icon',
+          },
+        ],
+      },
     ]
   })
   console.log('Message sent:', text)
