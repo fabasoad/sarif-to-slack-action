60405cdb75bb75de0f42578e0ec3169bf781915f
fabasoad <fabasoad@gmail.com>
2025-05-07 23:49:45 +0900

    Test webhook

--- src/__tests__/index.spec.ts
+++
@@ -1,5 +0,0 @@
-describe('index.ts', () => {
-  test('test', () => {
-    expect(true).toBe(true)
-  })
-})
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
+import { getInput } from '@actions/core'
+import { IncomingWebhook } from '@slack/webhook'
+import { promises } from 'fs'
+
 async function run() {
-  console.log('Test')
+  const webhookUrl = getInput('slack-webhook')
+  const sarifPath = getInput('sarif-path')
+  const jsonString = await promises.readFile(sarifPath, 'utf8')
+  const sarif = JSON.parse(jsonString)
+  const webhook = new IncomingWebhook(webhookUrl)
+  const { text } = await webhook.send({
+    text: 'Test'
+  })
+  console.log('Message sent:', text)
 }
 
 run()
