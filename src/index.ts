485544cb4b227dfc8ab03f878fe2d1bbf75b1087
fabasoad <fabasoad@gmail.com>
2025-05-07 23:56:08 +0900

    Test webhook 2

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { getInput } from '@actions/core'
 import { IncomingWebhook } from '@slack/webhook'
-import { promises } from 'fs'
+import { promises as fs } from 'fs'
 
 async function run() {
   const webhookUrl = getInput('slack-webhook')
   const sarifPath = getInput('sarif-path')
-  const jsonString = await promises.readFile(sarifPath, 'utf8')
-  const sarif = JSON.parse(jsonString)
+  // const jsonString = await fs.readFile(sarifPath, 'utf8')
+  // const sarif = JSON.parse(jsonString)
   const webhook = new IncomingWebhook(webhookUrl)
   const { text } = await webhook.send({
     text: 'Test'
