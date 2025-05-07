90cb4335a5d0fd156606a228d34c662b4d2cbf5a
fabasoad <fabasoad@gmail.com>
2025-05-08 00:04:42 +0900

    Test webhook 3

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,31 @@
 import { getInput } from '@actions/core'
 import { IncomingWebhook } from '@slack/webhook'
-import { promises as fs } from 'fs'
+// import { promises as fs } from 'fs'
 
 async function run() {
-  const webhookUrl = getInput('slack-webhook')
-  const sarifPath = getInput('sarif-path')
+  const webhookUrl: string = getInput('slack-webhook')
+  const sarifPath: string = getInput('sarif-path')
+  const color: string = getInput('color')
   // const jsonString = await fs.readFile(sarifPath, 'utf8')
   // const sarif = JSON.parse(jsonString)
   const webhook = new IncomingWebhook(webhookUrl)
   const { text } = await webhook.send({
-    text: 'Test'
+    blocks: [
+      {
+        type: 'rich_text',
+        elements: [
+          {
+            type: 'rich_text_section',
+            elements: [
+              {
+                type: 'color',
+                value: color
+              }
+            ]
+          }
+        ]
+      }
+    ]
   })
   console.log('Message sent:', text)
 }
