2918ba750477440dff4904600aebba9e81ebbbd9
fabasoad <fabasoad@gmail.com>
2025-05-31 23:33:27 +0900

    Add support for directory

--- src/__tests__/Processors.spec.ts
+++ src/__tests__/Processors.spec.ts
@@ -5,27 +5,27 @@ jest.mock('fs')
 const mockedFs = fs as jest.Mocked<typeof fs>
 
 describe('processColor', () => {
-  it('returns correct hex for success', () => {
+  test('returns correct hex for success', () => {
     expect(processColor('success')).toBe('#008000')
   })
 
-  it('returns correct hex for failure', () => {
+  test('returns correct hex for failure', () => {
     expect(processColor('failure')).toBe('#ff0000')
   })
 
-  it('returns correct hex for cancelled', () => {
+  test('returns correct hex for cancelled', () => {
     expect(processColor('cancelled')).toBe('#0047AB')
   })
 
-  it('returns correct hex for skipped', () => {
+  test('returns correct hex for skipped', () => {
     expect(processColor('skipped')).toBe('#808080')
   })
 
-  it('returns input for unknown color', () => {
+  test('returns input for unknown color', () => {
     expect(processColor('other')).toBe('other')
   })
 
-  it('returns undefined for undefined input', () => {
+  test('returns undefined for undefined input', () => {
     expect(processColor(undefined)).toBeUndefined()
   })
 })
@@ -38,12 +38,12 @@ describe('processSarifPath', () => {
     jest.resetAllMocks()
   })
 
-  it('throws if path does not exist', () => {
+  test('throws if path does not exist', () => {
     mockedFs.existsSync.mockReturnValue(false)
     expect(() => processSarifPath(fakeFile)).toThrow(/does not exist/)
   })
 
-  it('returns .sarif files in directory', () => {
+  test('returns .sarif files in directory', () => {
     mockedFs.existsSync.mockReturnValue(true)
     mockedFs.statSync.mockReturnValue({ isDirectory: () => true, isFile: () => false } as any)
     // @ts-ignore: mocking readdirSync with a specific return value
@@ -52,7 +52,7 @@ describe('processSarifPath', () => {
     expect(result).toEqual(['a.sarif', 'b.SARIF'])
   })
 
-  it('returns file path if it is a file', () => {
+  test('returns file path if it is a file', () => {
     mockedFs.existsSync.mockReturnValue(true)
     mockedFs.statSync.mockReturnValue({ isDirectory: () => false, isFile: () => true } as any)
     const result: string[] = processSarifPath(fakeFile)
@@ -60,7 +60,7 @@ describe('processSarifPath', () => {
     expect(result[0]).toEqual(fakeFile)
   })
 
-  it('throws if path is neither file nor directory', () => {
+  test('throws if path is neither file nor directory', () => {
     mockedFs.existsSync.mockReturnValue(true)
     mockedFs.statSync.mockReturnValue({ isDirectory: () => false, isFile: () => false } as any)
     expect(() => processSarifPath('/weird/path')).toThrow(/neither a file nor a directory/)
