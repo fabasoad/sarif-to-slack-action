1ce3fce161b11670ecaeaf97fda9ec489ba38ba0
fabasoad <fabasoad@gmail.com>
2025-08-15 15:32:22 +0900

    chore(deps): bump @fabasoad/sarif-to-slack from 0.2.5 to 1.1.0

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,46 @@
 import { getBooleanInput, getInput } from '@actions/core'
-import { SarifToSlackService } from '@fabasoad/sarif-to-slack'
+import { Color, SarifToSlackClient } from '@fabasoad/sarif-to-slack'
 import {
-  processCalculateResultsBy,
-  processGroupResultsBy,
-  processLogLevel
+  processLogLevel,
+  processRepresentationType,
+  processSarifExtension,
+  processSendIf
 } from './Processors'
 
 export async function run() {
-  const sarifToSlackService: SarifToSlackService = await SarifToSlackService.create({
+  const client: SarifToSlackClient = await SarifToSlackClient.create({
     webhookUrl: getInput('slack-webhook', { required: true, trimWhitespace: true }),
     username: getInput('username', { required: false, trimWhitespace: true }),
     iconUrl: getInput('icon-url', { required: false, trimWhitespace: true }),
-    color: getInput('color', { required: false, trimWhitespace: true }),
-    sarifPath: getInput('sarif-path', { required: true, trimWhitespace: true }),
+    color: {
+      default: new Color(getInput('color', { required: false, trimWhitespace: true })),
+      byLevel: {
+        error: new Color(getInput('color-level-error', { required: false, trimWhitespace: true })),
+        warning: new Color(getInput('color-level-warning', { required: false, trimWhitespace: true })),
+        note: new Color(getInput('color-level-note', { required: false, trimWhitespace: true })),
+        none: new Color(getInput('color-level-none', { required: false, trimWhitespace: true })),
+        unknown: new Color(getInput('color-level-unknown', { required: false, trimWhitespace: true })),
+        empty: new Color(getInput('color-level-empty', { required: false, trimWhitespace: true })),
+      },
+      bySeverity: {
+        critical: new Color(getInput('color-severity-critical', { required: false, trimWhitespace: true })),
+        high: new Color(getInput('color-severity-high', { required: false, trimWhitespace: true })),
+        medium: new Color(getInput('color-severity-medium', { required: false, trimWhitespace: true })),
+        low: new Color(getInput('color-severity-low', { required: false, trimWhitespace: true })),
+        none: new Color(getInput('color-severity-none', { required: false, trimWhitespace: true })),
+        unknown: new Color(getInput('color-severity-unknown', { required: false, trimWhitespace: true })),
+        empty: new Color(getInput('color-severity-empty', { required: false, trimWhitespace: true })),
+      },
+    },
+    sarif: {
+      path: getInput('sarif-path', { required: true, trimWhitespace: true }),
+      recursive: getBooleanInput('sarif-recursive', { required: true, trimWhitespace: true }),
+      extension: processSarifExtension(getInput('sarif-file-extension', { required: true, trimWhitespace: true })),
+    },
     log: {
       level: processLogLevel(getInput('log-level', { required: false, trimWhitespace: true })),
       template: getInput('log-template', { required: false, trimWhitespace: false }),
-      colored: false,
+      colored: getBooleanInput('log-colored', { required: false, trimWhitespace: false }),
     },
     header: {
       include: getBooleanInput('include-header', { required: false }),
@@ -33,12 +57,10 @@ export async function run() {
     run: {
       include: getBooleanInput('include-run', { required: false })
     },
-    output: {
-      groupBy: processGroupResultsBy(getInput('group-by', { required: false, trimWhitespace: true })),
-      calculateBy: processCalculateResultsBy(getInput('calculate-by', { required: false, trimWhitespace: true })),
-    }
+    representation: processRepresentationType(getInput('representation', { required: false, trimWhitespace: true })),
+    sendIf: processSendIf(getInput('send-if', { required: false, trimWhitespace: true })),
   })
-  await sarifToSlackService.sendAll()
+  await client.send()
 }
 
 run()
